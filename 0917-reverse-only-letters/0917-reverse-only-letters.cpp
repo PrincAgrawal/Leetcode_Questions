df31110ec5f178236a5c91534ea4694ceab1a76f
Prince Agrawal <111682628+PrincAgrawal@users.noreply.github.com>
2023-08-06 22:05:54 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (13.21%) - LeetHub

---
+++ 0917-reverse-only-letters/0917-reverse-only-letters.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    string reverseOnlyLetters(string s) {
+        int l=0;
+        int h=s.length()-1;
+        while(l<=h)
+        {
+            if(isalpha(s[l]) && isalpha(s[h]))
+            {
+                swap(s[l],s[h]);
+                l++;
+                h--;
+            }
+            else if(isalpha(s[l]))
+            {
+                h--;
+            }
+            else{
+                l++;
+            }
+        }
+        return s;
+    }
+};
