acd5aa19859c5a3ad596be2e932ec295671aaf02
Prince Agrawal <111682628+PrincAgrawal@users.noreply.github.com>
2023-09-27 18:00:38 +0530

    Time: 11 ms (84.18%), Space: 13.5 MB (31.70%) - LeetHub

--- 0532-k-diff-pairs-in-an-array/0532-k-diff-pairs-in-an-array.cpp
+++ 0532-k-diff-pairs-in-an-array/0532-k-diff-pairs-in-an-array.cpp
@@ -1,65 +1,32 @@
 class Solution {
 public:
- bool binary_search(vector<int> v,int s, int x)
-        {
-            int e= v.size()-1;
-            int mid= s+(e-s)/2;
-
-            while(s<=e)
-            {
-                if(v[mid]==x)
-                {
-                    return true;
-                }
-                else if(v[mid]>x)
-                {
-                    e= mid-1;
-                }
-                else{
-                    s= mid+1;
-                }
-                mid= s+(e-s)/2;
-            }
-            return false;
-        }
     int findPairs(vector<int>& nums, int k) {
-        // sort(nums.begin(),nums.end());
-        // set<pair<int,int>> ans;
-        // int i=0,j=1;
-
-        // while(j < nums.size())
-        // {
-        //     int diff= nums[j]-nums[i];
-        //     if(diff==k)
-        //     {
-        //         ans.insert({nums[i],nums[j]});
-        //         i++,j++;
-        //     }
-        //     else if(diff>k)
-        //     {
-        //        i++; 
-        //     }
-        //     else{
-        //         j++;
-        //     }
-        //     if(i==j)
-        //     {
-        //         j++;
-        //     }
-        // }
-        // return ans.size();
         sort(nums.begin(),nums.end());
-        int i=0;
-        set<pair<int,int>> ans;
-        while(i<nums.size())
-        {
-            int find= k+nums[i];
-            if(binary_search(nums,i+1,find))
-            {
-               ans.insert({nums[i],nums[i]+k});
-            }
-             i++;
+      
+      int i=0;
+      int j=1;
+      
+      set<pair<int,int>>ans;
+      
+      while(j<nums.size())
+      {
+        int diff=nums[j]-nums[i];
+        
+        if(diff==k){
+          ans.insert({nums[i],nums[j]});
+          i++,j++;
         }
-        return ans.size();
+        else if(diff>k){
+          i++;
+        }
+        else{
+          j++;
+        }
+        
+        if(i==j)
+          j++;
+      }
+      
+      return ans.size();
     }
 };
