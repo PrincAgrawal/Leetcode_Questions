b2c3252598c42da55396c6ed6c5a9b473f736de0
Prince Agrawal <111682628+PrincAgrawal@users.noreply.github.com>
2023-10-10 21:52:45 +0530

    Time: 4 ms (39.62%), Space: 8 MB (68.97%) - LeetHub

---
+++ 1748-sum-of-unique-elements/1748-sum-of-unique-elements.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int sumOfUnique(vector<int>& nums) {
+        int hash[101]={0};
+      for(int i=0;i<nums.size();i++)
+      {
+        hash[nums[i]]++;
+      }
+      int sum=0;
+      for(int i=0;i<=100;i++)
+      {
+        if(hash[i]==1)
+        {
+          sum+=i;
+        }
+      }
+      return sum;
+    }
+};
