9f12bb3c31f987a26f22dc1ac2fd03def59a05cb
Prince Agrawal <111682628+PrincAgrawal@users.noreply.github.com>
2023-10-15 12:35:41 +0530

    Time: 0 ms (100.00%), Space: 7.1 MB (27.75%) - LeetHub

--- 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.cpp
+++ 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.cpp
@@ -1,61 +1,13 @@
 class Solution {
 public:
-    
-    void removeOccure(string &s, string part){
-        int found = s.find(part);
-        
-        if(found!=string::npos)
-        {
-            string left_substr = s.substr(0,found);
-            string right_substr = s.substr(found+part.length(),s.size()-1);
-            s= left_substr + right_substr;
-        
-        }
-        else{
-            return;
-        }
-        
-        removeOccure(s,part);
-        
-    }
     string removeOccurrences(string s, string part) {
-        
-    // Method-1 --> 
-        
-        // int pos= s.find(part);
-        //  while(pos!= string::npos)
-        // {
-        //     s.erase(pos,part.length());
-        //      pos= s.find(part);
-        // } 
-        // return s;
-        
-   //Method-2 -->      
-        removeOccure(s,part);
-        return s;
-        
-        
-        //  this is my own incomplete solution..-->
-        
-        // int ele = part.length();
-        // int i=0;
-        // int j;
-        // while(i<s.length())
-        // {
-        //     for(int j=i;j<ele;j++)
-        //     {
-        //         if(s[j]!=part[j])
-        //          break;
-        //     }
-        //     if(j==ele)
-        //     {
-        //         s.erase(i,ele);
-        //         i=0;
-        //         // i=j;
-        //     }
-        //     else
-        //         i++;
-        // }
-        // return s;
+        int pos=s.find(part);
+      while(pos!=string::npos)
+      {
+        s.erase(pos,part.length());
+        pos=s.find(part);
+      }
+      
+      return s;
     }
 };
