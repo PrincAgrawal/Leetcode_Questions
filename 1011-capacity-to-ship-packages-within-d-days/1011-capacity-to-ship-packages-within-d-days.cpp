4e3e699828dcca2099157c171e21eec7b17ddf30
Prince Agrawal <111682628+PrincAgrawal@users.noreply.github.com>
2023-10-21 21:44:06 +0530

    Time: 89 ms (5.08%), Space: 56.6 MB (6.28%) - LeetHub

---
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+   bool isPossible(vector<int>arr,int days,int sol)
+  {
+      int countdays=1;
+      int weightsum=0;
+      for(int i=0;i<arr.size();i++)
+      {
+          if(arr[i]>sol)
+           return false;
+          else{
+              if(weightsum+arr[i]>sol)
+                {
+                    countdays++;
+                    weightsum=arr[i];
+                    if(countdays>days)
+                      return false;
+                }
+                else{
+                    weightsum+=arr[i];
+                }
+          }
+      }
+      return true;
+  }
+  
+    int shipWithinDays(vector<int>& arr, int days) {
+        if(days>arr.size()) 
+         return -1;
+        int s=0;
+        int e=accumulate(arr.begin(),arr.end(),0);
+        int ans=-1;
+        while(s<=e)
+        {
+            int mid=s+(e-s)/2;
+            if(isPossible(arr,days,mid))
+            {
+                ans=mid;
+                e=mid-1;
+            }
+            else{
+              s=mid+1;
+            }
+        }
+        return ans;
+    }
+};
